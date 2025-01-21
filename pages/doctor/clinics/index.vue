ae765eaaf32097b6854e785900c7876dffef2e32
Merhan Mostafa <merhanmostafa47@gmail.com>
2025-01-21 14:09:32 +0200

    Update doctor folder structure

--- pages/doctor/clinics/index.vue
+++ pages/doctor/clinics/index.vue
@@ -1,15 +1,45 @@
 <script setup>
 import { useI18n } from "vue-i18n";
-const { t } = useI18n();
+const { t, locale } = useI18n();
+const localePath = useLocalePath();
+
+// const { data: clinicList, pending: clinicListRequestIsPending } =
+//   await useBaseFetch("GET", "clinics", locale);
+
+// console.log(clinicList.value);
 </script>
 <template>
-  <div>
-    <DocSidebar />
-    <!-- Static Sidebar -->
-    <v-main class="max-h-screen mx-3 overflow-y-auto lg:mx-6">
-      <TopBar :title="t('TITLES.doctor.clinics')" />
-      <!-- Top bar with title -->
-      <nuxt-page /><!-- Dynamically render child pages here -->
-    </v-main>
+  <div class="px-5 content__wrapper">
+    <div class="flex items-center justify-between mb-4">
+      <BaseSearchInput placeholder="Search by clinic name and Doctor name " />
+      <NuxtLink class="btn main-btn" :to="localePath('/doctor/clinics/add')">
+        add a new clinic
+        <Icon name="material-symbols:add" size="20" />
+      </NuxtLink>
+    </div>
+    <div class="table__wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th id="clinic-name">Clinic name</th>
+            <th id="doctor-name">Doctor name</th>
+            <th id="phone-number">Clinic phone number</th>
+            <th id="working-hours">Working hours</th>
+            <th id="speciality">Clinic speciality</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Clinic name</td>
+            <td>Doctor name</td>
+            <td>Clinic phone number</td>
+            <td>Working hours</td>
+            <td>Clinic speciality</td>
+            <td>:</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
