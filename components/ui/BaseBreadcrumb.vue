fe064c3d1345a2b655c5faff2b316d36d7dcbea8
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2025-02-01 18:48:17 +0200

    Add: breadcrumb component

---
+++ components/ui/BaseBreadcrumb.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="breadcrumb_wrapper">
+    <v-breadcrumbs :items="breadcrumbItems" :divider="divider"></v-breadcrumbs>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  breadcrumbItems: {
+    type: Array,
+    required: true,
+  },
+  divider: {
+    type: String,
+    default: '/',
+  },
+});
+</script>
+
+<style lang="postcss">
+.breadcrumb_wrapper {
+    @apply pb-4;
+  .v-breadcrumbs {
+    @apply flex flex-wrap p-0;
+    .v-breadcrumbs-item {
+      @apply text-gray-clr;
+      opacity: .5;
+      &:hover {
+        opacity: 1;
+      }
+      &--disabled {
+        opacity: 1;
+      }
+      &--active {
+      @apply text-main-clr;
+      }
+      &--link {
+        &:hover {
+          text-decoration: none;
+        }
+      }
+    }
+    .v-breadcrumbs-divider {
+      @apply text-gray-clr;
+      opacity: .5;
+    }
+  }
+}
+</style>
