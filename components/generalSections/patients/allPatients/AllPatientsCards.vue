9cf5d02cd3f078724adcb083629b72068e2a0536
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2025-02-01 18:44:47 +0200

    Add: All Patients data (Table and Cards)

---
+++ components/generalSections/patients/allPatients/AllPatientsCards.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="(item, index) in allPatients"
+      :key="index"
+      cols="12" sm="6" md="4" lg="3"
+      class="patient-card"
+    >
+      <v-card
+        :prepend-avatar="item.image_url || '/media/icons/user-placeholder.png'"
+        :title="item.name"
+        :subtitle="item.user_id"
+        class="!rounded-[1rem] !shadow-lg"
+      >
+        <v-card-text>
+          <div class="mb-3"><p class="text-secondary-clr">Age</p><p class="text-main-clr font-bold">{{ item.dob }}</p></div>
+          <div class="mb-3"><p class="text-secondary-clr">Gender</p><p class="text-main-clr font-bold">{{ item.gender }}</p></div>
+          <div class="mb-3"><p class="text-secondary-clr">Phone</p><p class="text-main-clr font-bold">{{ item.phone }}</p></div>
+        </v-card-text>
+        <v-card-actions>
+          <NuxtLink class="p-4 text-center bg-opacity-bg text-main-clr w-full !rounded-xl hover:!bg-main-clr hover:!text-white transition duration-300" :to="localePath(`/doctor/patients/${item.user_id}`)">View Details</NuxtLink>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+const patientsStore = usePatientsStore();
+const  { allPatients, pagination } = storeToRefs(patientsStore);
+</script>
+
+<style lang="postcss">
+  .patient-card .v-card {
+    &-title {
+      @apply text-main-clr;
+    }
+    &-subtitle {
+      @apply text-secondary-clr;
+    }
+  }
+</style>
--- pages/doctor/patients/index.vue
+++ pages/doctor/patients/index.vue
@@ -1,3 +1,99 @@
 <template>
-    patients
+  <v-container>
+    <section class="flex flex-wrap items-center justify-between gap-2 mb-4">
+      <BaseSearchInput placeholder="Search by patient ID" @search="search = $event" />
+      <div class="flex items-center gap-3">
+        <div
+          class="flex justify-space-around border-main-clr border rounded-md overflow-hidden"
+        >
+          <Icon
+            @click="toggleView"
+            name="material-symbols:lists"
+            :class="[
+              'p-[2px] cursor-pointer transition',
+              isTableView ? 'bg-main-clr text-white' : 'bg-white text-main-clr',
+            ]"
+            size="1.5rem"
+          />
+          <Icon
+            @click="toggleView"
+            name="material-symbols:cards"
+            :class="[
+              'p-[2px] cursor-pointer transition',
+              !isTableView
+                ? 'bg-main-clr text-white hover:bg-opacity-bg'
+                : 'bg-white text-main-clr',
+            ]"
+            size="1.5rem"
+          />
+        </div>
+        <NuxtLink class="btn main-btn" :to="localePath('/doctor/patients/add')">
+          Add a new patient
+          <Icon name="material-symbols:add" size="20" />
+        </NuxtLink>
+      </div>
+    </section>
+
+    <section v-if="allPatients.length">
+      <allPatientsTable v-if="isTableView" />
+      <allPatientsCards v-else />
+      <v-pagination
+        @update:modelValue="updateRouterQueryParam"
+        v-model="page"
+        :length="pagination.last_page"
+        :total-visible="pagination.per_page"
+      >
+      </v-pagination>
+    </section>
+  </v-container>
 </template>
+
+<script setup>
+const { locale } = useI18n();
+const router = useRouter();
+const route = useRoute();
+
+const page = ref(parseInt(route.query.page) || 1);
+
+const isTableView = ref(true);
+const toggleView = () => {
+  isTableView.value = !isTableView.value;
+};
+
+const patientsStore = usePatientsStore();
+const { allPatients, pagination } = storeToRefs(patientsStore);
+
+const { data, error } = await useBaseFetch("GET", "patients", locale, null, {
+  page: page.value,
+});
+if (!error.value) {
+  patientsStore.setAllPatientsData(data.value);
+}
+
+const updateRouterQueryParam = async (newPage) => {
+  await router.push({
+    query: {
+      ...route.query,
+      page: newPage,
+    },
+  });
+
+  try {
+    const res = await useClientFetch("GET", "patients", locale.value, null, {
+      page: page.value,
+    });
+    patientsStore.setAllPatientsData(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(async () => {
+  await router.push({
+    query: {
+      ...route.query,
+      page: page.value,
+    },
+  });
+});
+</script>
