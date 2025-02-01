043f3431a86fe4b4bf39a1836c6491c62a00878f
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2025-02-01 18:40:37 +0200

    Update: provide toast for nuxtapp to use it directly without import

--- plugins/toast.js
+++ plugins/toast.js
@@ -1,10 +1,13 @@
 // toast.js
 import Toast ,{ POSITION }  from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import { useToast } from "vue-toastification";
+
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Toast, {
     hideProgressBar: true,
     position: POSITION.BOTTOM_LEFT,
   });
+  nuxtApp.provide("toast", useToast());
 });
